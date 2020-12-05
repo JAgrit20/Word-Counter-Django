52856dea99b92f544e6964822d49f91105eb3100
JAgrit20 <jagritacharya2020@gmail.com>
2020-12-06 01:30:46 +0530

    word counter complete

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 
 def homepage(request):
@@ -8,9 +9,19 @@ def homepage(request):
 def count(request):
     fulltext = request.GET['fullText']
     # print(fulltext)
-    wordlist = fulltext.split();
+    wordlist = fulltext.split()
 
-    return render(request,'count.html',{'fullText':fulltext,'count':len(wordlist)})
+    worddict = {}
+
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] +=1
+        else:
+            worddict[word] =1
+
+    sortedwords =  sorted(worddict.items(),key=operator.itemgetter(1),reverse=True)
+
+    return render(request,'count.html',{'fullText':fulltext,'count':len(wordlist),'worddict':sortedwords})
 
     # def homepage(request):
     # return render(request,'home.html',{"hithere":'this is jagrit'})
