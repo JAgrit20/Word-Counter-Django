64d485ffb9497cec8dfc233786b412a12aad2d09
JAgrit20 <jagritacharya2020@gmail.com>
2020-12-06 00:22:21 +0530

    basic template added

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse('hello world')
+
+def homepage(request):
+    return render(request,'home.html')
